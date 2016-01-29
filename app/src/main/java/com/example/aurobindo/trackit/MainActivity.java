3c01f8a995c38b1ab4f43a6de9359ec23fe83769
aurobindoc <aurobindoc1991@gmail.com>
2016-01-30 00:34:30 +0530

    Accelerometer module done; Started with Gyyroscope

--- app/src/main/java/com/example/aurobindo/trackit/MainActivity.java
+++ app/src/main/java/com/example/aurobindo/trackit/MainActivity.java
@@ -20,37 +20,41 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private boolean toggle = false;
     private SensorManager sensormanager = null;
     private File externalStorageDirectory;
-    private TextView dispAcc = (TextView) findViewById(R.id.tv_acc);
-    private Button track = (Button) findViewById(R.id.button_Track);
-    private Button stop = (Button) findViewById(R.id.button_Stop);
+    private FileWriter filewriterAcc, filewriterGyro;
+    private TextView dispAcc, dispGyro;
+    private Button track, stop;
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         sensormanager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         externalStorageDirectory = Environment.getExternalStorageDirectory();
 
-
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        dispAcc = (TextView) findViewById(R.id.tv_acc);
+        dispGyro = (TextView) findViewById(R.id.tv_gyro);
+        track = (Button) findViewById(R.id.button_Track);
+        stop = (Button) findViewById(R.id.button_Stop);
+        try {
+            filewriterAcc = new FileWriter(new File(externalStorageDirectory, "accelerometer_"+dateFormat.format(new Date())+".tsv"), true);
+            filewriterGyro = new FileWriter(new File(externalStorageDirectory, "gyroscope_"+dateFormat.format(new Date())+".tsv"), true);
+        } catch (IOException e) {}
     }
 
     @Override
@@ -68,14 +72,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+    float x, y, z;
+        /**************************** Accelerometer ***************************/
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
             if(toggle)  {
                 dispAcc.setText("x=" + x + "\ty=" + y + "\tz=" + z);
-            }
 
+                try {
+                    filewriterAcc.write("\n" + dateFormat.format(new Date()) + "\t" + x + "\t" + y + "\t" + z);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /***************************** Gyroscope ******************************/
+       if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+            if(toggle)  {
+                dispAcc.setText("x=" + x + "\ty=" + y + "\tz=" + z);
+
+                try {
+                    filewriterGyro.write("\n" + dateFormat.format(new Date()) + "\t" + x + "\t" + y + "\t" + z);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -86,11 +113,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensormanager.registerListener(this,
                 sensormanager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_FASTEST);
+        sensormanager.registerListener(this,
+                sensormanager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     public void trackStop (View view) {
         toggle = false;
         sensormanager.unregisterListener(this);
+        try {
+            filewriterAcc.close();
+            filewriterGyro.close();
+        } catch (IOException e) {}
     }
 
     @Override
